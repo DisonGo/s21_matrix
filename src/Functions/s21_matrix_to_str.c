43382b128ec9e46cb8f9051139ccbd010670cf4f
evverenn <evverenn@student.21-school.ru>
2022-12-01 16:56:03 +0300

    develop. matrix_to_str done.

---
+++ src/Functions/s21_matrix_to_str.c
@@ -0,0 +1,28 @@
+#include "s21_matrix.h"
+#include <stdlib.h>
+#include "string.h"
+/**
+ * @brief Creates table of matrix in str with given precision.
+ * 
+ * @param A Source matrix.
+ * @param precision Presicion of values. (0 = no mantissa).
+ * @return New str with parsed matrix.
+ * @retval char* - New str.
+ * @retval NULL - Memory error.
+ */
+char *s21_matrix_to_str(matrix_t *A, int precision) {
+    int row_size = A->columns * (precision ? (precision + 3) : 2);
+    char* str = calloc(A->rows * row_size + 1, sizeof(char));
+    if (!str) return NULL;
+    for(int y = 0; y < A->rows; y++) {
+        char tmp[1024] = {0};
+        for(int x = 0; x < A->columns; x++) {
+            int next_not_end = (x + 1 < A->columns);
+            sprintf(tmp, "%*.*lf", precision + 1, precision, A->matrix[y][x]);
+            if (next_not_end) strncat(tmp, " ", 1);
+            strncat(str, tmp, precision + 2 + next_not_end);
+        }
+        strncat(str, "\n", 1);
+    }
+    return str;
+}
