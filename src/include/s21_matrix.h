e9583ad6ddeff3d8be01f7e717f9344d39f7de50
evverenn <evverenn@student.21-school.ru>
2022-12-01 19:36:05 +0300

    develop. Utility upd.

--- src/include/s21_matrix.h
+++ src/include/s21_matrix.h
@@ -26,4 +26,5 @@ int s21_calc_complements(matrix_t *A, matrix_t *result);
 int s21_determinant(matrix_t *A, double *result);
 int s21_inverse_matrix(matrix_t *A, matrix_t *result);
 char *s21_matrix_to_str(matrix_t *A, int precision);
+#include "Utility.h"
 #endif  // S21_MATRIX_H_
--- src/other/include/Utility.h
+++ src/other/include/Utility.h
@@ -1,9 +1,13 @@
 #ifndef UTILITY_H
 #define UTILITY_H
+#include <math.h>
+#include "s21_matrix.h"
 int max_i(int a, int b);
 int min_i(int a, int b);
 double max_d(double a, double b);
 double min_d(double a, double b);
 long double max_ld(long double a, long double b);
 long double min_ld(long double a, long double b);
+int is_eq_ld(long double a, long double b, long double precision);
+int is_eq_mat_size(matrix_t *a, matrix_t *b);
 #endif  // UTILITY_H
