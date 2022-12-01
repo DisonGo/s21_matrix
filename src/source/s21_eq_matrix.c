88671b2be4e5e37dcb8715e663340544e17bec5e
evverenn <evverenn@student.21-school.ru>
2022-12-01 19:35:33 +0300

    develop. Eq_matrix done.

--- src/source/s21_eq_matrix.c
+++ src/source/s21_eq_matrix.c
@@ -1,6 +1,19 @@
 #include "s21_matrix.h"
-//TODO Implement eq_matrix
+/**
+ * @brief Determine if matrices are equal.
+ * 
+ * @param A First matrix.
+ * @param B Second matrix.
+ * @return Bool - Are matrices equal.
+ * @retval int
+ */
 int s21_eq_matrix(matrix_t *A, matrix_t *B) { 
-
-  return 1;
+  if (!A || !B) return 0;
+  if (!is_eq_mat_size(A, B)) return 0;
+  int x = A->columns, y = A->rows;
+  int res = 1;
+  for (int i = 0; i < y; i++)
+    for (int j = 0; j < x; j++)
+      if (!is_eq_ld(A->matrix[i][j], B->matrix[i][j], 1E-7)) res = 0;
+  return res;
 }
