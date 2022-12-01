e4d7fdff84d7aae6ec4d6d3f32f6acb36396d5b8
evverenn <evverenn@student.21-school.ru>
2022-12-01 19:36:05 +0300

    develop. Utility upd.

--- src/other/source/Utility.c
+++ src/other/source/Utility.c
@@ -8,3 +8,14 @@ double min_d(double a, double b) {return a < b ? a : b;}
 
 long double max_ld(long double a, long double b) {return a > b ? a : b;}
 long double min_ld(long double a, long double b) {return a < b ? a : b;}
+
+int is_eq_ld(long double a, long double b, long double precision) {
+    a = (long double)(float) a;
+    b = (long double)(float) b;
+    long double diff = fabs(fabs(a) - fabs(b));
+    return (diff <= precision);
+}
+
+int is_eq_mat_size(matrix_t *a, matrix_t *b) {
+    return (a->columns == b->columns && a->rows == b->rows);
+}
