0296ccaac9ce885c61f6290cf4e4c3e59899e674
evverenn <evverenn@student.21-school.ru>
2022-12-01 18:11:32 +0300

    develop. Project structure upgrade.

---
+++ src/other/include/Utility.h
@@ -0,0 +1,9 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+int max_i(int a, int b);
+int min_i(int a, int b);
+double max_d(double a, double b);
+double min_d(double a, double b);
+long double max_ld(long double a, long double b);
+long double min_ld(long double a, long double b);
+#endif  // UTILITY_H
