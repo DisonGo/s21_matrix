a889394307b764a40e17eb0cd4816a5d27b9a181
evverenn <evverenn@student.21-school.ru>
2022-12-02 12:01:30 +0300

    develop. Remove_matrix fixes.

--- src/source/s21_remove_matrix.c
+++ src/source/s21_remove_matrix.c
@@ -9,7 +9,7 @@ void s21_remove_matrix(matrix_t *A) {
   if (!A) return;
   if (A->matrix) {
     for(int i = 0; i < A->rows; i++)
-      if (A->matrix[i]) free(A->matrix[i]);
+      free(A->matrix[i]);
     free(A->matrix);
   }
 }
