835a3f54ace6bf793c0727c008cae19a5f97ae47
evverenn <evverenn@student.21-school.ru>
2022-12-02 12:02:36 +0300

    develop. Sub_matrix done.

--- src/source/s21_sub_matrix.c
+++ src/source/s21_sub_matrix.c
@@ -1,5 +1,24 @@
 #include "s21_matrix.h"
-//TODO Implement sub_matrix
+/**
+ * @brief Substruct matrices.
+ * 
+ * @param A First matrix.
+ * @param B Second matrix.
+ * @param result Output matrix.
+ * @return Success of function.
+ * @retval 0 - OK.
+ * @retval 1 - Memory, incorrect matrix errors.
+ * @retval 2 - Wrong size.
+ */
 int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) { 
-  return 1;
+  if (is_null_mat(A) || is_null_mat(B)) return 1;
+  if (!is_eq_mat_size(A, B)) return 2;
+  int rows = A->rows, cols = A->columns;
+  if (s21_create_matrix(rows, cols, result)) return 1;
+  for (int row = 0; row < rows; row++)
+    for (int col = 0; col < cols; col++) {
+      long double sub = A->matrix[row][col] - B->matrix[row][col];
+      result->matrix[row][col] = sub;
+    }
+  return 0;
 }
