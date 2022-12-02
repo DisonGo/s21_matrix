1ba27dbf05caad00d656bfb2ebd13c503b2853f3
evverenn <evverenn@student.21-school.ru>
2022-12-02 12:00:50 +0300

    develop. Mult_number done.

--- src/source/s21_mult_number.c
+++ src/source/s21_mult_number.c
@@ -1,5 +1,22 @@
 #include "s21_matrix.h"
-//TODO Implement mult_number
-int s21_mult_number(matrix_t *A, double number, matrix_t *result) { 
+/**
+ * @brief Multiply matrix by number.
+ * 
+ * @param A Matrix.
+ * @param number Multiplication number.
+ * @param result Output matrix.
+ * @return Success of function.
+ * @retval 0 - OK.
+ * @retval 1 - Memory, incorrect matrix errors.
+ */
+int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
+  if (is_null_mat(A)) return 1;
+  int rows = A->rows, cols = A->columns;
+  if (s21_create_matrix(rows, cols, result)) return 1;
+  for (int row = 0; row < rows; row++)
+    for (int col = 0; col < cols; col++) {
+      long double mult = A->matrix[row][col] * number;
+      result->matrix[row][col] = mult;
+    }
   return 1;
 }
