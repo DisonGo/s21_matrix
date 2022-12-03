6a8f3d168ddca264aeded7bd2aea753fd1c9b23c
evverenn <evverenn@student.21-school.ru>
2022-12-03 22:51:01 +0300

    develop. Changed return of eq_matrix to macro.

--- src/source/s21_eq_matrix.c
+++ src/source/s21_eq_matrix.c
@@ -11,9 +11,9 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
   if (!A || !B) return 0;
   if (!is_eq_mat_size(A, B)) return 0;
   int x = A->columns, y = A->rows;
-  int res = 1;
+  int res = SUCCESS;
   for (int i = 0; i < y; i++)
     for (int j = 0; j < x; j++)
-      if (!is_eq_ld(A->matrix[i][j], B->matrix[i][j], 1E-7)) res = 0;
+      if (!is_eq_ld(A->matrix[i][j], B->matrix[i][j], 1E-7)) res = FAILURE;
   return res;
 }
