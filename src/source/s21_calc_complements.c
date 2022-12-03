bd6941d6e714e56373e5f1c560f36d54145d60e2
evverenn <evverenn@student.21-school.ru>
2022-12-03 22:50:01 +0300

    develop. Calc_complement done.

--- src/source/s21_calc_complements.c
+++ src/source/s21_calc_complements.c
@@ -1,5 +1,36 @@
 #include "s21_matrix.h"
-//TODO Implement calc_complements
-int s21_calc_complements(matrix_t *A, matrix_t *result) { 
-  return 1;
+#include <math.h>
+
+// TODO Refactor
+/**
+ * @brief Calculate matrix cofactor matrix.
+ * 
+ * @param A Source matrix.
+ * @param result Output matrix.
+ * @return Success of function.
+ * @retval 0 - OK.
+ * @retval 1 - Memory, incorrect matrix errors.
+ * @retval 2 - Wrong size.
+ */
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+  if (is_null_mat(A) || !is_null_mat(result)) return 1;
+  if (A->rows != A->columns) return 2;
+  if (s21_create_matrix(A->rows, A->columns, result)) return 1;
+  if (A->rows == 2) {
+    result->matrix[0][0] = A->matrix[1][1];
+    result->matrix[0][1] = -A->matrix[1][0];
+    result->matrix[1][0] = -A->matrix[0][1];
+    result->matrix[1][1] = A->matrix[0][0];
+  } else {
+    int stop = 0;
+    for (int i = 0; i < result->rows && !stop; i++) {
+      for (int j = 0; j < result->columns && !stop; j++) {
+        double temp = get_minor(A, i, j);
+        if (!isinf(fabs(temp)) && !isnan(fabs(temp))) {
+          result->matrix[i][j] = temp * pow(-1, i + j);
+        }
+      }
+    }
+  }
+  return 0;
 }
