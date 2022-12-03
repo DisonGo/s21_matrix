d917f241cda3fd0d107b097aaa468a721ec8b3bb
evverenn <evverenn@student.21-school.ru>
2022-12-03 22:48:53 +0300

    develop. Transpose done.

--- src/source/s21_transpose.c
+++ src/source/s21_transpose.c
@@ -1,5 +1,18 @@
 #include "s21_matrix.h"
-//TODO Implement transpose
+/**
+ * @brief Switch around rows and columns of matrix into new one.
+ * 
+ * @param A Source matrix.
+ * @param result Output matrix.
+ * @return Success of function.
+ * @retval 0 - OK.
+ * @retval 1 - Memory, incorrect matrix errors.
+ */
 int s21_transpose(matrix_t *A, matrix_t *result) { 
-  return 1;
+  if (is_null_mat(A) || !is_null_mat(result)) return 1;
+  if (s21_create_matrix(A->columns, A->rows, result)) return 1;
+  for (int col = 0; col < A->columns; col++)
+    for (int row = 0; row < A->rows; row++)
+      result->matrix[col][row] = A->matrix[row][col];
+  return 0;
 }
