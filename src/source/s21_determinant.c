96ed652f416b511fef24bc754860d75340efd4ee
evverenn <evverenn@student.21-school.ru>
2022-12-03 22:52:08 +0300

    develop. Determinant done.

--- src/source/s21_determinant.c
+++ src/source/s21_determinant.c
@@ -1,5 +1,25 @@
 #include "s21_matrix.h"
-//TODO Implement determinant
-int s21_determinant(matrix_t *A, double *result) { 
-  return 1;
+/**
+ * @brief Calculate determinant of matrix.
+ * 
+ * @param A Input matrix.
+ * @param result Output value.
+ * @return Success of function.
+ * @retval 0 - OK.
+ * @retval 1 - Memory, incorrect matrix errors.
+ * @retval 2 - Wrong size.
+ */
+int s21_determinant(matrix_t *A, double *result) {
+  if (is_null_mat(A)) return 1;
+  if (A->columns != A->rows) return 2;
+  int size = A->rows;
+  if (size == 2) *result = determinant_2x2(A);
+  if (size != 2) {
+    for (int col = 0; col < size; col++) {
+      long double cur_number = A->matrix[0][col];
+      int sign = (col % 2 == 0) ? 1 : -1;
+      *result += cur_number ? sign * cur_number * get_minor(A, 0, col) : 0;
+    }
+  }
+  return 0;
 }
