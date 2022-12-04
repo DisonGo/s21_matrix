9b43cd54bc487a50d354aca1e6882d733c659dec
evverenn <evverenn@student.21-school.ru>
2022-12-04 15:10:19 +0300

    develop. Commented unused funcs.

--- src/other/source/Utility.c
+++ src/other/source/Utility.c
@@ -1,13 +1,13 @@
 #include "Utility.h"
 
-int max_i(int a, int b) { return a > b ? a : b; }
-int min_i(int a, int b) { return a < b ? a : b; }
+// int max_i(int a, int b) { return a > b ? a : b; }
+// int min_i(int a, int b) { return a < b ? a : b; }
 
-double max_d(double a, double b) { return a > b ? a : b; }
-double min_d(double a, double b) { return a < b ? a : b; }
+// double max_d(double a, double b) { return a > b ? a : b; }
+// double min_d(double a, double b) { return a < b ? a : b; }
 
-long double max_ld(long double a, long double b) { return a > b ? a : b; }
-long double min_ld(long double a, long double b) { return a < b ? a : b; }
+// long double max_ld(long double a, long double b) { return a > b ? a : b; }
+// long double min_ld(long double a, long double b) { return a < b ? a : b; }
 
 int is_eq_ld(long double a, long double b, long double precision) {
   // a = (long double)(float) a;
