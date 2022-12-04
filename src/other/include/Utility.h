e316de7910c0a46d9ee7e5251ffb6688fdeefb1a
evverenn <evverenn@student.21-school.ru>
2022-12-04 14:55:29 +0300

    develop. New utils. Styles.

--- src/other/include/Utility.h
+++ src/other/include/Utility.h
@@ -1,6 +1,7 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 #include <math.h>
+
 #include "s21_matrix.h"
 int max_i(int a, int b);
 int min_i(int a, int b);
@@ -9,7 +10,9 @@ double min_d(double a, double b);
 long double max_ld(long double a, long double b);
 long double min_ld(long double a, long double b);
 int is_eq_ld(long double a, long double b, long double precision);
-int is_eq_mat_size(matrix_t *a, matrix_t *b);
-int is_null_mat(matrix_t *a);
+int is_eq_mat_size(matrix_t* a, matrix_t* b);
+int is_null_mat(matrix_t* a);
+int is_incorrect_mat(matrix_t* a);
+int is_bad_size_mat(matrix_t* a);
 double determinant_2x2(matrix_t* A);
 #endif  // UTILITY_H
